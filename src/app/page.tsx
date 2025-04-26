60f7167203bd5cf6695ddd568341c64435764ca9
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-04-26 06:16:49 -0300

    feat: add threed marquee and images on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,44 @@
 import { CTA } from "@/components/ui/cta";
+import { ThreeDMarquee } from "@/components/ui/threed-marquee";
 
 export default function Home() {
+  const images = [
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
+    "https://images.unsplash.com/photo-1582192730841-2a682d7375f9",
+    "https://images.unsplash.com/photo-1582192903020-8a5e59dcdcf2",
+    "https://images.unsplash.com/photo-1637073849640-b283dcd9a111",
+    "https://images.unsplash.com/photo-1624377632657-3902bfd35958",
+    
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
+    "https://images.unsplash.com/photo-1582192730841-2a682d7375f9",
+    "https://images.unsplash.com/photo-1582192903020-8a5e59dcdcf2",
+    "https://images.unsplash.com/photo-1637073849640-b283dcd9a111",
+    "https://images.unsplash.com/photo-1624377632657-3902bfd35958",
+
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
+    "https://images.unsplash.com/photo-1582192730841-2a682d7375f9",
+    "https://images.unsplash.com/photo-1582192903020-8a5e59dcdcf2",
+    "https://images.unsplash.com/photo-1637073849640-b283dcd9a111",
+    "https://images.unsplash.com/photo-1624377632657-3902bfd35958",
+
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
+    "https://images.unsplash.com/photo-1582192730841-2a682d7375f9",
+    "https://images.unsplash.com/photo-1582192903020-8a5e59dcdcf2",
+    "https://images.unsplash.com/photo-1637073849640-b283dcd9a111",
+    "https://images.unsplash.com/photo-1624377632657-3902bfd35958",
+
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
+    "https://images.unsplash.com/photo-1582192730841-2a682d7375f9",
+    "https://images.unsplash.com/photo-1582192903020-8a5e59dcdcf2",
+    "https://images.unsplash.com/photo-1637073849640-b283dcd9a111",
+    "https://images.unsplash.com/photo-1624377632657-3902bfd35958",
+  ];
+
   return (
     <div className="relative mx-auto flex h-screen w-full max-w-7xl flex-col items-center justify-center overflow-hidden rounded-3xl">
       <h2 className="relative z-20 mx-auto max-w-4xl text-center text-4xl font-bold text-balance text-white md:text-4xl lg:text-6xl">
@@ -15,6 +53,12 @@ export default function Home() {
       </p>
 
       <CTA />
+
+      <div className="absolute inset-0 z-10 h-full w-full bg-black/80 dark:bg-black/40" />
+      <ThreeDMarquee
+        className="pointer-events-none absolute inset-0 h-full w-full"
+        images={images}
+      />
     </div>
   );
 }
